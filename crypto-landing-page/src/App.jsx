aa4daf6cc637f451859571cc80cceaadd69a27f2
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-09-09 20:43:46 +0530

    new: crypto-landing-page

---
+++ crypto-landing-page/src/App.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
